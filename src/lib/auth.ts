9d62c2f9be959dab67120b6120ec51a33fb7a332
cfbeg <210145573+cfbeg@users.noreply.github.com>
2025-09-03 23:08:34 +0900

    Better Authのinit

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { twoFactor } from 'better-auth/plugins'
+import { passkey } from 'better-auth/plugins/passkey'
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [
+        nextCookies(),
+        twoFactor(),
+        passkey()
+    ],
+});
